796effc1e9ffc61d3bb1f4c2be10e47ea099203e
abdessamadtanafaat <tanafaatabdessamad@gmail.com>
2025-02-12 13:06:55 +0100

    darkmode

--- assets/js/newsletter.js
+++ assets/js/newsletter.js
@@ -1,9 +1,9 @@
-document.addEventListener("DOMContentLoaded", function () {
-    fetch("/components/newsletter.html")
-        .then(response => response.text())
-        .then(html => {
-            document.getElementById("newsletter").innerHTML = html;
-            initImageSlider(); // Call the image slider function after loading HTML
-        })
-        .catch(error => console.error("Error loading hero section:", error));
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//     fetch("/components/newsletter.html")
+//         .then(response => response.text())
+//         .then(html => {
+//             document.getElementById("newsletter").innerHTML = html;
+//             initImageSlider(); // Call the image slider function after loading HTML
+//         })
+//         .catch(error => console.error("Error loading hero section:", error));
+// });
